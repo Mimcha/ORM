fc7450a3a613f42f24a6c1bfb31459aa6c7f8ea0
MIM <mim-oza86@mail.ru>
2025-02-18 11:29:20 +0300

    first commit

--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -5,20 +5,23 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-import java.io.Serializable;
-
 @Entity
 @Table(name = "PERSONS")
-public class Person implements Serializable {
+public class Person {
 
     @EmbeddedId
-    private PersonKey id; // Составной первичный ключ
+    private PersonKey id;
 
     @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
     @Column(name = "city_of_living", nullable = false)
     private String cityOfLiving;
+
+    // Геттер для доступа к возрасту через PersonKey
+    public int getAge() {
+        return id.getAge();
+    }
 
     // Геттеры и сеттеры
     public PersonKey getId() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Ivan', 'Ivanov', 30, '1234567890', 'Moscow');
 INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Petr', 'Petrov', 25, '9876543210', 'Saint Petersburg');
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Sidor', 'Sidorov', 35, '5555555555', 'Moscow');
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Alexey', 'Smirnov', 28, '1111111111', 'Moscow');
