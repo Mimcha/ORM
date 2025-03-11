53ebccd2cde1e939e3b0b8a902c107f1e6f2f85e
MIM <mim-oza86@mail.ru>
2025-03-11 08:52:24 +0300

    first commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,33 +1,37 @@
 package com.example.demo.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable() // Отключение CSRF для упрощения тестирования
+                .csrf().disable()
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/persons/city/**", "/api/persons/younger-than/**").permitAll() // Доступ без авторизации
-                        .anyRequest().authenticated() // Все остальные endpoint'ы требуют авторизации
+                        .requestMatchers("/api/persons/city/**", "/api/persons/younger-than/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults()) // Включение стандартной формы логина
-                .logout(Customizer.withDefaults()) // Включение возможности выхода
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // Используется для тестирования
+        return NoOpPasswordEncoder.getInstance();
     }
 }
